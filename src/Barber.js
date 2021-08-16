6439a12b1ee7efbe208b745f2416c9591f98bfdc
segunsub <segunsubair3@gmail.com>
2021-08-16 12:58:21 -0400

    barber page

--- src/Barber.js
+++ src/Barber.js
@@ -1,9 +1,17 @@
+import {Image} from 'react-bootstrap'
+import barb from './clipper.jpeg'
 
 function Barber() {
     return (
-        <div>
-            csdcdscsd
-        </div>
+        <div class="boxContain">
+        <Image src={barb} alt="cleaning image" fluid rounded style={{width: '99vw'}}/>
+        <h4 className='textDiv'>TreeStump
+            <em className='title'>
+            Barbing page
+            </em>
+        </h4>
+        
+     </div>
     )
 }
 
