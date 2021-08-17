fa9ab413a65f105b21bc698993198475de1a55f3
segunsub <segunsubair3@gmail.com>
2021-08-17 09:13:53 -0400

    minor change

--- src/Barber.js
+++ src/Barber.js
@@ -3,15 +3,18 @@ import barb from './clipper.jpeg'
 
 function Barber() {
     return (
-        <div class="boxContain">
-        <Image src={barb} alt="cleaning image" fluid rounded style={{width: '99vw'}}/>
-        <h4 className='textDiv'>TreeStump
-            <em className='title'>
-            Barbing page
-            </em>
-        </h4>
-        
-     </div>
+        <>
+            <div class="boxContain">
+            <Image src={barb} alt="cleaning image" fluid rounded style={{width: '99vw'}}/>
+            <h4 className='textDiv'>Barber Shop
+                <em className='title'>
+                Create Your Style
+                </em>
+            </h4>
+            
+        </div>
+        <h2>Come In For A Fresh Cut</h2>
+     </>
     )
 }
 
